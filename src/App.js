1db06d7eb7acdd630fd0a55c743facb9f2344a70
you he <heyou88@hotmail.com>
2023-07-25 12:27:08 +0200

    replace the icon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 // import logo from './logo.svg';
 import React, { useEffect,useState } from 'react';
 import './App.css';
-import mute from './mute.png';
-import tree from './tree.png';
-import save from './save.png';
-import share from './share.png';
+import muteButton from './mute.png';
+import muteicon from './mute.svg';
+import unmuteicon from './unmute.svg';
+import unmuteButton from './unmute.png';
+import tree from './merge tree.png';
+import save from './star.svg';
+import share from './share.svg';
 import avatar from './avatar.png';
+import speakers from './speakers.png';
+import soundOn from './soundon.png';
+import soundOff from './soundoff.png';
 
 function App() {
   const soundURL = 'https://vocal-belekoy-a2bce4.netlify.app/chat.mp3';
-  const [muted, setMuted] = useState(true);
+  const [sound, setSound] = useState(true);
+  const [mute, setMute] = useState(true);
   useEffect(() => {
     // Play the sound automatically when the component mounts
     const audio = new Audio(soundURL);
-    if (!muted) {
+    if (!sound) {
       audio.play();
     } else {
       audio.pause();
@@ -22,60 +29,51 @@ function App() {
 
     // Optionally, you can also pause the sound when the component unmounts
     return () => audio.pause();
-  }, [muted, soundURL]);
+  }, [sound, soundURL]);
+  const handleToggleSound = () => {
+    setSound(!sound);
+  };
   const handleToggleMute = () => {
-    setMuted(!muted);
+    setMute(!mute);
   };
   return (
     <div className="App">
-    <header className="App-header">
-      <div className="tree-name-container">
-        <h3 className="tree-name">Tree Name</h3>
-        <img src={save} className="save" alt="Save" />
-        <img src={share} className="share" alt="Share" />
-        <img src={avatar} className="avatar" alt="Avatar" />
-      </div>
+      <header className="App-header">
+        <div className="tree-name-container">
+          <h3 className="tree-name">Tree Name</h3>
+          <img src={save} className="save" alt="Save" />
+          <img src={share} className="share" alt="Share" />
+          <img src={avatar} className="avatar" alt="Avatar" />
+        </div>
 
         <img src={tree} className="tree" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <div>
-          <div className='speakers'>
-            <div className='speaker'>
-               <img src={avatar} className="speaker-avatar" alt="Avatar" />
-               <p>Abdul</p>
-            </div>
-            <div className='speaker'>
-               <img src={avatar} className="speaker-avatar" alt="Avatar" />
-               <p>Abdul</p>
-            </div>
-            <div className='speaker'>
-               <img src={avatar} className="speaker-avatar" alt="Avatar" />
-               <p>Abdul</p>
-            </div>
-            <div className='speaker'>
-               <img src={avatar} className="speaker-avatar" alt="Avatar" />
-               <p>Abdul</p>
-            </div>
-          </div>
+        <div className="speakers">
+          <img
+            src={sound ? soundOn : soundOff}
+            className="sound-button"
+            onClick={handleToggleSound}
+            alt="mute"
+          />
+          <img src={speakers} className="speaker-avatar" alt="logo" />
         </div>
-        <div className='bottom'>
-        <div className="mute" onClick={handleToggleMute}>
-            {muted ? 'MUTE' : 'UNMUTE'}
-            <img src={mute} className="toggle-button" alt="mute" />
+
+        <div className="bottom">
+          <div className="mute">
+            <img
+              src={mute ? muteicon : unmuteicon}
+              className="mute-icon"
+              alt="mute"
+            />
+            <img
+              src={mute ? muteButton : unmuteButton}
+              className="toggle-button"
+              onClick={handleToggleMute}
+              alt="mute"
+            />
           </div>
-            <div>
-              <button className='leave-button'>leave</button>
-            </div>
+          <div>
+            <button className="leave-button">Leave</button>
+          </div>
         </div>
       </header>
     </div>
